c18b3f979a92652944c45646306981404d3a44ee
Boyoung Kim <bokim@z4r2p5.42lyon.fr>
2026-02-18 15:22:02 +0100

    testing

--- README.md
+++
@@ -1,3 +0,0 @@
-# 42-libft
-Detailed guide to the first project of 42 common core
-
--- test_ft_strlen.c
+++ test_ft_strlen.c
@@ -1,2 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test_ft_strlen.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bokim <bokim@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/18 15:20:22 by bokim             #+#    #+#             */
+/*   Updated: 2026/02/18 15:21:57 by bokim            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <string.h>
 #include <stdio.h>
+#include "libft.h"
+
+int main(void)
+{
+    char    *str;
+    int     expected;
+    int     actual;
+    
+    str = "test";
+    expected = strlen(str);
+    actual = ft_strlen(str);
+    if (expected == actual)
+        return (0);
+    else
+        return (1);
+}
